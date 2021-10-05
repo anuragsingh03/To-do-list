2fc94d40ffea715d72fb364d5cefb5f31d2b943c
anurag <sanurag4342@gmail.com>
2021-10-05 14:07:18 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,82 @@
+let inputdata=document.getElementById("input");
+let addbutton=document.getElementById("add");
+let addevent=document.getElementById("addevent");
+
+
+//add event
+let countaddid=0;
+let countdeleteid=1000;
+
+addbutton.addEventListener('click',function()
+{
+    if(inputdata.value!='')
+    {
+    let createlist=document.createElement('li');
+
+    createlist.innerHTML=inputdata.value;
+    let btndelete = document.createElement("BUTTON"); 
+    btndelete.setAttribute('id',countdeleteid); 
+    btndelete.setAttribute('onclick','performdeletion(id)');
+    
+btndelete.innerHTML = "delete";                   
+createlist.appendChild(btndelete);    
+    let btn = document.createElement("BUTTON"); 
+    btn.setAttribute('id',countaddid);
+btn.innerHTML = "check"; 
+btn.setAttribute('onclick','performcheck(id)');                  
+createlist.appendChild(btn); 
+addevent.appendChild(createlist);
+inputdata.value='';
+countaddid++;
+countdeleteid++;
+
+}
+
+})
+
+//Not completed box
+//check box
+let countdeleteid2=2000;
+let completed=document.getElementById('completedevent');
+function performcheck(id)
+{
+    let change=document.getElementById(id);//selecting list from id
+    let list=document.createElement('li');
+    
+    let x=change.parentNode.innerText;
+    let y=x.split(/\r?\n/);
+   // console.log( y);
+    list.innerHTML=y[0];
+   
+    //list.innerHTML
+   
+    let btndelete = document.createElement("BUTTON"); 
+    btndelete.setAttribute('id',countdeleteid2); 
+    btndelete.setAttribute('onclick','performdeletion2(id)');
+    btndelete.innerHTML = "delete"; 
+    list.appendChild(btndelete);
+    completed.appendChild(list);
+    
+    // change.parentNode.removeChild(change.parentNode.firstChild);
+    // change.parentNode.removeChild(change.parentNode.firstChild);
+    change.parentNode.remove();
+    countdeleteid2++;
+
+    
+
+}
+
+//delete box
+function performdeletion(id)
+{
+    let change=document.getElementById(id);
+    change.parentNode.remove();
+}
+// //complete box
+function performdeletion2(id){
+    let change=document.getElementById(id);
+    change.parentNode.remove();
+
+}
+
+
